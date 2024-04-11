49f598de7b55940f36fdf914e277372763f99572
Roman Ožana <roman@ozana.cz>
2024-04-11 15:08:03 +0200

    Update easter

--- test/day-meta.ts
+++ test/day-meta.ts
@@ -4,34 +4,24 @@ import {DateTime} from "luxon";
 
 
 test("Christmas Eve in 2024", t => {
-	t.deepEqual(getDayMeta(DateTime.fromISO('2024-12-24')),
-		{
-			names: ['Adam', 'Eva'],
-			easter: undefined,
-			isSignificantDay: false,
-			significantDay: undefined,
-			isPublicHoliday: true,
-			publicHoliday: 'Štědrý den',
-			shops: {areOpen: true, status: 'otevřeno do 12:00'}
-		}
-	);
+	const meta = getDayMeta(DateTime.fromISO('2024-12-24'));
+	t.is(meta.publicHoliday, 'Štědrý den');
+
+	t.true(meta.shops.areOpen);
+	t.is(meta.shops.status, 'otevřeno do 12:00');
 });
 
 test('Easter Monday in 2024', t => {
-	t.deepEqual(getDayMeta(DateTime.fromISO('2024-04-01')),
-		{
-			names: ['Hugo'],
-			easter: {
-				name: 'Velikonoční pondělí',
-				isGoodFriday: false,
-				isHolySaturday: false,
-				isHolySunday: false,
-				isEasterMonday: true
-			},
-			isSignificantDay: false,
-			significantDay: undefined,
-			isPublicHoliday: true,
-			publicHoliday: 'Velikonoční pondělí',
-			shops: {areOpen: false, status: 'zavřeno'}
-		});
+	const meta = getDayMeta(DateTime.fromISO('2024-04-01'));
+
+
+	if (meta.easter) {
+		t.is(meta.easter.name, 'Velikonoční pondělí');
+		t.is(meta.easter.isGoodFriday, false);
+		t.is(meta.easter.isHolySaturday, false);
+		t.is(meta.easter.isHolySunday, false);
+		t.is(meta.easter.isEasterMonday, true);
+	}
+	t.is(meta.publicHoliday, 'Velikonoční pondělí');
+	t.false(meta.shops.areOpen);
 });
