22ad0770ce53e53e792bae3ded0339141152bc67
Roman Ožana <roman@ozana.cz>
2024-04-12 10:20:09 +0200

    Fix eslint error

--- lib/easter.ts
+++ lib/easter.ts
@@ -37,13 +37,13 @@ export function getEasterSunday(year: number): DateTime {
 	const e = b % 4; // eslint-disable-line unicorn/prevent-abbreviations
 	const f = Math.floor((b + 8) / 25);
 	const g = Math.floor((b - f + 1) / 3);
-	const h = (19 * a + b - d - g + 15) % 30;
+	const h = ((19 * a) + b - d - g + 15) % 30;
 	const i = Math.floor(c / 4);
 	const k = c % 4;
-	const l = (32 + 2 * e + 2 * i - h - k) % 7;
-	const m = Math.floor((a + 11 * h + 22 * l) / 451);
-	const month = Math.floor((h + l - 7 * m + 114) / 31);
-	const day = ((h + l - 7 * m + 114) % 31) + 1;
+	const l = (32 + (2 * e) + (2 * i) - h - k) % 7;
+	const m = Math.floor((a + (11 * h) + (22 * l)) / 451);
+	const month = Math.floor((h + l - (7 * m) + 114) / 31);
+	const day = ((h + l - (7 * m) + 114) % 31) + 1;
 
 	return DateTime.fromObject({year, month, day});
 }
