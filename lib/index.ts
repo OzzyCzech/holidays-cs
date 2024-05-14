8624ffe628061acffbb442922de909cca476c167
Roman Ožana <roman@ozana.cz>
2024-05-14 09:34:08 +0200

    Move Mothers and Fathers day to
    https://github.com/OzzyCzech/international-days-cs

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,5 @@
 export * from './easter.js';
 export * from './holidays.js';
-export * from './fathers-day.js';
-export * from './mothers-day.js';
 export * from './significant.js';
 export * from './shops.js';
 export * from './get-meta.js';
